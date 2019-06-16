8a6626ea046b80cf9eaa5744d802da4aefe9a623
daviddomingues <david.daviddomingues@outlook.com>
2019-06-16 03:57:33 -0300

    retirar adicionar/editar/excluir vagas

--- src/pages/lista-vaga/lista-vaga.ts
+++ src/pages/lista-vaga/lista-vaga.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { VagaProvider } from '../../providers/vaga/vaga';
-import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
@@ -12,19 +11,16 @@ export class ListaVagaPage {
 
   vagas = [];
   listaPadrao = [];
-  admin = false;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public provedor: VagaProvider,
     public alerCtrl: AlertController,
-    public auth: AuthProvider
     ) {
   }
   
   ionViewWillEnter(){
-    this.admin = this.auth.logado();
     this.chamaListar();
   }
 
@@ -39,28 +35,6 @@ export class ListaVagaPage {
     this.navCtrl.push('MostraVagaPage',{id : id});
   }
 
-  novaVaga() {
-    this.navCtrl.push('CadastroVagaPage');
-  }
-
-  editar(id){
-    this.navCtrl.push('CadastroVagaPage',{id: id});
-  }
-
-  excluir(id) {
-    let alert = this.alerCtrl.create();
-    alert.setTitle('Tem certeza que deseja excluir?');
-    alert.addButton('Cancelar');
-    alert.addButton({
-      text: 'Ok',
-      handler: data => {
-        this.provedor.excluir(id);
-        this.chamaListar();
-      }
-    });
-    alert.present().then(() => {
-    });
-  }
 
   filtrarItens(event) {
     var pesquisado = event.target.value;
