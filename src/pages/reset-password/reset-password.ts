6e0b66016e61bbd90ef52e476a6c9df52170c322
David Domingues <david@uniarp.edu.br>
2019-06-27 10:32:05 -0300

    Login na lista do menu/ Resetar senha funcionando / Tratamento de e-mail vazio no resetar senha

---
+++ src/pages/reset-password/reset-password.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-reset-password',
+  templateUrl: 'reset-password.html',
+})
+
+export class ResetPasswordPage {
+
+  email: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public authservice: AuthProvider,
+    public alertCtrl: AlertController
+    ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ResetPasswordPage');
+  }
+
+  resetPassword() {
+    if (this.email == null){
+      let alert = this.alertCtrl.create();
+          alert.setTitle('Erro: nenhum e-mail informado.');
+          alert.addButton('Ok');
+          alert.present().then(() => {
+          });
+    } else {
+      this.authservice.resetPassword(this.email).then((res: any) => {
+      
+        if (!res.code){
+  
+        }else
+          alert(res);
+      })
+    }
+  
+  }
+
+}
