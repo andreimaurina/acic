e59443a9f03961fb360fc4ac12de54e3bf2e17d6
DIEGO SICKA RODRIGUES <diegosicka@hotmail.com>
2019-06-28 20:59:59 -0300

    Adicionado abas SWIPE no Sobre

--- src/pages/sobre/sobre.ts
+++ src/pages/sobre/sobre.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { SobreProvider } from '../../providers/sobre/sobre';
 
 @IonicPage()
@@ -8,6 +8,10 @@ import { SobreProvider } from '../../providers/sobre/sobre';
   templateUrl: 'sobre.html',
 })
 export class SobrePage {
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+
+  SwipedTabsIndicator :any= null;
+  tabs:any=[];
 
   sobres = [];
   editando = false;
@@ -17,6 +21,29 @@ export class SobrePage {
      public navParams: NavParams,
      public provedor: SobreProvider
     ) {
+      this.tabs=["História","Visão","Missão"];
+  }
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+  }
+
+  selectTab(index) {    
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+    this.SwipedTabsSlider.slideTo(index, 500);
+  }
+
+  updateIndicatorPosition() {
+      // this condition is to avoid passing to incorrect index
+  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
+  	{
+  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+  	}
+    
+    }
+
+  animateIndicator($event) {
+  	if(this.SwipedTabsIndicator)
+   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
   }
 
   ionViewWillEnter(){
