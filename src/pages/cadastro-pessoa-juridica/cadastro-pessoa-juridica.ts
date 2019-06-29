c4281168611ea084240f4eeff3b23db5a9497eed
Andrei Maurina <andreimaurinagcb@hotmail.com>
2019-06-28 21:55:44 -0300

    pop cadastro repetido pessoa juridica

--- src/pages/cadastro-pessoa-juridica/cadastro-pessoa-juridica.ts
+++ src/pages/cadastro-pessoa-juridica/cadastro-pessoa-juridica.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AssociadoProvider } from '../../providers/associado/associado';
 import { PessoaJuridica } from '../../models/Associado';
 
@@ -12,7 +12,12 @@ export class CadastroPessoaJuridicaPage {
   id = null;
   associado: PessoaJuridica;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public provedor :AssociadoProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public provedor :AssociadoProvider,
+    public alertCtrl: AlertController
+    ) {
     this.id = this.navParams.data.id;
     if (!this.id) {
       this.associado = new PessoaJuridica();
@@ -29,8 +34,44 @@ export class CadastroPessoaJuridicaPage {
   }
 
   gravar(id){
-    this.provedor.gravar(this.associado,id);
-    this.navCtrl.pop();
+    if(id){
+      this.provedor.gravar(this.associado,id);
+      this.navCtrl.pop();
+    }else{
+      this.verificar().then(
+        data => {
+          if (data){
+            this.provedor.gravar(this.associado,id);
+            this.navCtrl.pop();
+          } else {
+            let alert = this.alertCtrl.create();
+            alert.setTitle('Atenção!');
+            alert.setSubTitle('Este associado já está cadastrado!');
+            alert.addButton('Ok');
+            alert.present().then(() => {
+            });
+          }
+        }
+      );
+    }
   }
+
+  verificar(){
+    return this.provedor.listar()
+    .then(
+      data => {
+        const listaAssociados = data;
+        const cnpj = this.associado.cnpj;
+        const associados = listaAssociados.filter((elemento) => {
+          if (elemento.cnpj==cnpj){
+            return true;
+          }
+          return false;
+        });
+        return associados.length == 0;
+      }
+    );
+  }
+
 
 }
