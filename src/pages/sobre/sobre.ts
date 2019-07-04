0e811c75e062de936e7368f6df8a5c1a439d6ccc
Pedro Henrique Paulek <pedroxhp@outlook.com>
2019-07-03 23:12:37 -0300

    sobre_quase_pronto

--- src/pages/sobre/sobre.ts
+++ src/pages/sobre/sobre.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { SobreProvider } from '../../providers/sobre/sobre';
+import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
@@ -9,52 +10,65 @@ import { SobreProvider } from '../../providers/sobre/sobre';
 })
 export class SobrePage {
   @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
-
+  
   SwipedTabsIndicator :any= null;
   tabs:any=[];
-
+  
   sobres = [];
+  admin = false;
   editando = false;
-
+  
   constructor(
     public navCtrl: NavController,
-     public navParams: NavParams,
-     public provedor: SobreProvider
+    public navParams: NavParams,
+    public provedor: SobreProvider,
+    public auth: AuthProvider,
     ) {
       this.tabs=["História","Visão","Missão","Diretoria"];
-  }
-  ionViewDidEnter() {
-    this.SwipedTabsIndicator = document.getElementById("indicator");
-  }
-
-  selectTab(index) {    
-    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
-    this.SwipedTabsSlider.slideTo(index, 500);
-  }
-
-  updateIndicatorPosition() {
-      // this condition is to avoid passing to incorrect index
-  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
-  	{
-  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
-  	}
-    
     }
-
-  animateIndicator($event) {
-  	if(this.SwipedTabsIndicator)
-   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
-  }
-
-  ionViewWillEnter(){
-    this.chamaListar();
-  }
-
-  chamaListar(){
-    this.provedor.listar()
-    .then(
-      data => this.sobres = data
-    );
-  }
-}
-
+    
+    ionViewDidEnter() {
+      this.SwipedTabsIndicator = document.getElementById("indicator");
+    }
+    
+    selectTab(index) {    
+      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+      this.SwipedTabsSlider.slideTo(index, 500);
+    }
+    
+    updateIndicatorPosition() {
+      // this condition is to avoid passing to incorrect index
+      if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
+      {
+        this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+      }
+      
+    }
+    
+    animateIndicator($event) {
+      if(this.SwipedTabsIndicator)
+      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
+    }
+    
+    chamaListar(){
+      this.provedor.listar()
+      .then(
+        data => this.sobres = data
+        );
+      }
+      
+      ionViewWillEnter(){
+        this.admin = this.auth.logado();
+        this.chamaListar();
+      }
+      
+      editar(status){
+        this.editando = status;
+      }
+      
+      gravar(id){
+        this.provedor.gravar(this.sobres[0],id);
+        this.editando = false ; 
+      }
+      
+    }
